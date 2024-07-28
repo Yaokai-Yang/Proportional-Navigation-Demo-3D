793f09f73537d92b1adf4d967f76bdf658eb6b3e
Yaokai Yang <yaokai.y@gmail.com>
2024-07-28 00:15:43 -0500

    changed implementation 'time to go' for ZEM

--- ProNav Demo/Assets/ZeroEffortMiss.cs
+++ ProNav Demo/Assets/ZeroEffortMiss.cs
@@ -7,11 +7,24 @@ using static UnityEngine.GraphicsBuffer;
 public class ZeroEffortMiss : ProNavBasics
 {
     public float time_to_go;
+
+    private void Awake()
+    {
+        // Vector3 relative_position = target.position - transform.position;
+        // Vector3 relative_velocity = target.GetComponent<ObjectController>().velocity - controller.velocity;
+        // time_to_go = relative_position.magnitude / relative_velocity.magnitude;
+    }
+
     protected override Vector3 doUpdate(float deltaTime)
     {
+        // time_to_go -= deltaTime;
+
         Vector3 relative_position = target.position - transform.position;
         Vector3 relative_velocity = target.GetComponent<ObjectController>().velocity - controller.velocity;
+        time_to_go = relative_position.magnitude / relative_velocity.magnitude;
         Vector3 zero_effort_miss = relative_position + relative_velocity * time_to_go;
+
+        Debug.DrawRay(transform.position, zero_effort_miss, Color.green, deltaTime);
 
         Vector3 LOS_unit = relative_position.normalized;                        // unit vector towards the target
         Vector3 ZEM_proj_LOS = Vector3.Project(zero_effort_miss, LOS_unit); 
